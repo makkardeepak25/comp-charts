ddb735025745907d22625e72001e9a1d935da15d
Deepak Makkar <33006240+makkardeepak25@users.noreply.github.com>
2024-01-06 19:09:47 +0530

    Update Dashboard.jsx
    Minor changes in code

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,28 +13,61 @@ const Dashboard = ({ missionsData }) => {
     { headerName: "Time", field: "time" },
     { headerName: "Rocket", field: "rocket" },
     { headerName: "Price", field: "price" },
-    { headerName: "Successful", field: "successful" }
+    { headerName: "Successful", field: "successful" },
   ];
 
-  const successfulMissionsCount = missionsData.filter(mission => mission?.successful)?.length;
-  const unsuccessfulMissionsCount = missionsData?.length - successfulMissionsCount;
+  const successfulMissionsCount = missionsData.filter(
+    (mission) => mission?.successful
+  )?.length;
+  // const successfulMissionsCountPer =
+  //   (successfulMissionsCount / missionsData?.length).toFixed(2) * 100;
+  const unsuccessfulMissionsCount =
+    missionsData?.length - successfulMissionsCount;
+  // const unSuccessfulMissionsCountPer =
+  //   (unsuccessfulMissionsCount / missionsData?.length).toFixed(2) * 100;
+  // const pieChartData = [
+  //   { name: "Successful", value: successfulMissionsCountPer },
+  //   { name: "Unsuccessful", value: unSuccessfulMissionsCountPer },
+  // ];
+  const totalMissions = successfulMissionsCount + unsuccessfulMissionsCount;
+  const successPercentage = (successfulMissionsCount / totalMissions) * 100;
+  const unsuccessPercentage = 100 - successPercentage;
+
   const pieChartData = [
-    { name: "Successful", value: successfulMissionsCount },
-    { name: "Unsuccessful", value: unsuccessfulMissionsCount }
+    { name: "Successful", value: successPercentage },
+    { name: "Unsuccessful", value: unsuccessPercentage },
   ];
 
   return (
     <div className="flex items-center justify-between w-full p-5">
-      <div className="ag-theme-alpine mb-5 w-1/2" style={{ height: '500px' }}>
+      <div
+        className="mb-5 ag-theme-alpine"
+        style={{ height: "700px", width: "40%" }}
+      >
         <AgGridReact columnDefs={columns} rowData={missionsData} />
       </div>
-      <div style={{ width: "50%", padding: "20px" }} className="flex flex-col gap-5 w-1/2 p-5 items-center">
-        <h2 className="font-bold text-center ">Missions Success/Failure Ratio</h2>
-        <PieChart width={400} height={400}>
-          <Pie dataKey="value" data={pieChartData} cx={200} cy={150} outerRadius={150} fill="#494866" label>
+      <div
+        style={{ padding: "20px" }}
+        className="flex flex-col items-center gap-5 p-5"
+      >
+        <h2 className="font-bold text-center ">
+          Missions by Status
+        </h2>
+        <PieChart width={700} height={400}>
+          <Pie
+            dataKey="value"
+            data={pieChartData}
+            cx={350}
+            cy={200}
+            outerRadius={120}
+            fill="#8884d8"
+            label={({ name, percent }) =>
+              `${name} ${(percent * 100).toFixed(2)}%`
+            }
+          >
             <Cell fill="#1b0fc4" />
             <Cell fill="#ff2828" />
-            <Legend />
+            <Legend fill="#1b0fc4" />
             <Tooltip />
           </Pie>
         </PieChart>
